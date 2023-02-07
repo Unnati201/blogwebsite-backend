011bbd0b6fdade8026e69df6fca44c4cd55e6857
Unnati Singh <113298408+Unnati201@users.noreply.github.com>
2023-02-07 12:31:12 +0530

    commit

--- connection.js
+++ connection.js
@@ -2,10 +2,10 @@ const { MongoClient } = require("mongodb");
 
 const connection = async () => {
   const client = await MongoClient.connect(
-    "mongodb+srv://uSingh:uSingh@cluster0.35udqsf.mongodb.net/blogging?retryWrites=true&w=majority"
+    "mongodb+srv://uSingh:uSingh@cluster0.35udqsf.mongodb.net/?retryWrites=true&w=majority"
   );
 
-  const db = client.db();
+  const db = client.db("blogging");
 
   return { db };
 };
